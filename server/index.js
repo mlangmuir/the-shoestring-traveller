91da3d89a9d5fbc39a7470331144ddca74faddcd
“Matthew <matthewlangmuir@hotmail.com>
2022-09-15 15:26:12 -0700

    development setup

--- server/index.js
+++ server/index.js
@@ -58,6 +58,15 @@ const app = express()
         app.use('/', express.static(__dirname + '/'))
     } else {
         const clientDistPath = path.resolve(__dirname, '../client/build');
+        app.get('*', (req, res, next) => {
+            if (req.path.startsWith('/api/')) {
+            next();
+    
+            return;
+            }
+    
+            res.sendFile(path.resolve(clientDistPath, 'index.html'));
+        });
         app.use('/', express.static(clientDistPath))
     }
 
